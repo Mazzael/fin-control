8ca9ffa507ec0e406b8bfe475d69c47c0deb0230
vmazza <vtrujillomazza@gmail.com>
2024-12-03 23:05:35 -0300

    feat: add current_fund property to user entity

--- src/domain/transactions/enterprise/entities/user.py
+++ src/domain/transactions/enterprise/entities/user.py
@@ -3,11 +3,13 @@ from datetime import datetime
 from pydantic import BaseModel, Field
 from src.core.entities.entity import Entity
 from src.core.entities.unique_entity_id import UniqueEntityID
+from src.domain.transactions.enterprise.entities.literals.transaction_type import TransactionType
 
 class UserPropsDict(TypedDict, total=False):
     name: str
     cpf: str
     password_hash: str
+    current_funds_in_cents: int
     created_at: datetime
     updated_at: Optional[datetime]
 
@@ -15,6 +17,7 @@ class UserProps(BaseModel):
     name: str
     cpf: str
     password_hash: str
+    current_funds_in_cents: int
     created_at: datetime = Field(default_factory=datetime.now)
     updated_at: Optional[datetime] = None
 
@@ -31,6 +34,10 @@ class User(Entity[UserProps]):
     @property
     def password_hash(self) -> str:
         return self.props.password_hash
+    
+    @property
+    def current_funds_in_cents(self) -> int:
+        return self.props.current_funds_in_cents
 
     @property
     def created_at(self) -> datetime:
@@ -45,6 +52,14 @@ class User(Entity[UserProps]):
 
     def change_password(self, new_password_hash: str):
         self.props.password_hash = new_password_hash
+        self._touch()
+
+    def update_current_fund(self, transaction_value_in_cents: int, transaction_type: TransactionType):
+        if transaction_type == 'Debit':
+            self.props.current_funds_in_cents -= transaction_value_in_cents
+        else:
+            self.props.current_funds_in_cents += transaction_value_in_cents
+
         self._touch()
 
     @staticmethod
